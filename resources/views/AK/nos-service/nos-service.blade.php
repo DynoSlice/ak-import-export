6bc91950743d040083ef29db25493d118460620b
DynoSlice <alphakaba5@gmail.com>
2020-11-11 21:19:30 +0100

    mise a jour meta seo

--- resources/views/AK/nos-service/nos-service.blade.php
+++ resources/views/AK/nos-service/nos-service.blade.php
@@ -1,6 +1,52 @@
-@extends('layouts.app')
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <!--====== Meta html ======-->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Ak import-Export, découvrez notre gamme variée de services destinés aux particuliers et professionnels" />
 
-@section('content')
+    <!--====== Titre de la page ======-->
+    <title>AK import-Export - découvrez nos services, du transport de marchandises à la manutention</title>
+
+    <!--====== ICON =======-->
+    <link rel="shortcut icon" type="image/png" href="/img/logoAK.png" />
+
+    <!--====== STYLESHEETS ======-->
+    <link rel="stylesheet" href="{{ asset('css/normalize.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/animate.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/stellarnav.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/owl.carousel.css')}}">
+    <link href="{{ asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('css/font-awesome.min.css')}}" rel="stylesheet">
+
+    <!--====== MAIN STYLESHEETS ======-->
+    <link href="{{ asset('css/style.css')}}" rel="stylesheet">
+    <link href="{{ asset('css/responsive.css')}}" rel="stylesheet">
+
+    <script src="{{ asset('js/vendor/modernizr-2.8.3.min.js')}}"></script>
+
+    <!--============= Link map =================== -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
+
+    <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="crossorigin=""></script>
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-169344018-2"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-169344018-2');
+        </script>
+    
+</head>
+
+<body class="home-one">
+    @extends('layouts.app')
+    @section('content')
 <header class="top-area single-page" id="home">
     <div class="top-area-bg-ns" data-stellar-background-ratio="0.6"></div>
     <div class="header-top-area">
--- resources/views/AK/nos-service/transport-routier.blade.php
+++ resources/views/AK/nos-service/transport-routier.blade.php
@@ -1,6 +1,52 @@
-@extends('layouts.app')
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <!--====== Meta html ======-->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Ak import-Export assure un service logistique, fret de marchandises ou transport frigorifique, depuis les grandes villes de Guinée" />
 
-@section('content')
+    <!--====== Titre de la page ======-->
+    <title>Transport routier de marchandises en Guinée - AK import-Export</title>
+
+    <!--====== ICON =======-->
+    <link rel="shortcut icon" type="image/png" href="/img/logoAK.png" />
+
+    <!--====== STYLESHEETS ======-->
+    <link rel="stylesheet" href="{{ asset('css/normalize.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/animate.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/stellarnav.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/owl.carousel.css')}}">
+    <link href="{{ asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('css/font-awesome.min.css')}}" rel="stylesheet">
+
+    <!--====== MAIN STYLESHEETS ======-->
+    <link href="{{ asset('css/style.css')}}" rel="stylesheet">
+    <link href="{{ asset('css/responsive.css')}}" rel="stylesheet">
+
+    <script src="{{ asset('js/vendor/modernizr-2.8.3.min.js')}}"></script>
+
+    <!--============= Link map =================== -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
+
+    <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="crossorigin=""></script>
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-169344018-2"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-169344018-2');
+        </script>
+    
+</head>
+
+<body class="home-one">
+    @extends('layouts.app')
+    @section('content')
 <header class="top-area single-page" id="home">
     <div class="top-area-bg-tr" data-stellar-background-ratio="0.6"></div>
     <div class="header-top-area">
@@ -14,7 +60,7 @@
             <div class="row flex-v-center">
                 <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12">
                     <div class="welcome-text text-center">
-                        <h1>Transport routier</h1>
+                        <h1>Transport routier GUINEE</h1>
                         <ul class="page-location">
                             <li><a href="#">Transport</a></li>
                             <li>/</li>

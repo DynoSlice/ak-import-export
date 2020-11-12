f7fe46f32ad6a8cbd5dcdfa117803e39b342a7ce
DynoSlice <alphakaba5@gmail.com>
2020-11-12 23:45:41 +0100

    optimisation javascript+image

--- resources/views/AK/nos-service/nos-service.blade.php
+++ resources/views/AK/nos-service/nos-service.blade.php
@@ -37,13 +37,9 @@
     
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-169344018-2"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'UA-169344018-2');
-        </script>
+    <script>
+        function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","UA-169344018-2");
+    </script>
     
 </head>
 

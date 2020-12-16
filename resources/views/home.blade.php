cc99f267e23d8e49d4b9ce37e7dbfb80be6d550e
DynoSlice <alphakaba5@gmail.com>
2020-12-16 16:30:50 +0100

    async script js

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,11 +28,11 @@
     <link href="{{ asset('css/style.css')}}" rel="stylesheet" media="all">
     <link href="{{ asset('css/responsive.css')}}" rel="stylesheet" media="all">
 
-    <script  rel="preload" defer src="{{ asset('js/vendor/modernizr-2.8.3.min.js')}}"></script>
+    <script async  rel="preload" src="{{ asset('js/vendor/modernizr-2.8.3.min.js')}}"></script>
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script  rel="preload"  async src="https://www.googletagmanager.com/gtag/js?id=UA-169344018-2"></script>
-    <script  rel="preload" defer>
+    <script  rel="preload" async>
         function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","UA-169344018-2");
     </script>
     
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,27 +9,27 @@
 
 
     <!--====== SCRIPTS JS ======-->
-    <script rel="preload" defer src="/js/vendor/jquery-1.12.4.min.js"></script>
-    <script rel="preload" defer src="/js/vendor/bootstrap.min.js"></script>
+    <script rel="preload" async src="/js/vendor/jquery-1.12.4.min.js"></script>
+    <script rel="preload" async src="/js/vendor/bootstrap.min.js"></script>
 
     <!--====== PLUGINS JS ======-->
-    <script rel="preload" defer src="/js/vendor/jquery.easing.1.3.js"></script>
-    <script rel="preload" defer src="/js/vendor/jquery-migrate-1.2.1.min.js"></script>
-    <script rel="preload" defer src="/js/vendor/jquery.appear.js"></script>
-    <script rel="preload" defer src="/js/owl.carousel.min.js"></script>
-    <script defer src="/js/stellar.js"></script>
-    <script rel="preload" defer src="/js/wow.min.js"></script>
-    <script rel="preload" defer src="/js/stellarnav.min.js"></script>
+    <script rel="preload" async src="/js/vendor/jquery.easing.1.3.js"></script>
+    <script rel="preload" async src="/js/vendor/jquery-migrate-1.2.1.min.js"></script>
+    <script rel="preload" async src="/js/vendor/jquery.appear.js"></script>
+    <script rel="preload" async src="/js/owl.carousel.min.js"></script>
+    <script async src="/js/stellar.js"></script>
+    <script rel="preload" async src="/js/wow.min.js"></script>
+    <script rel="preload" async src="/js/stellarnav.min.js"></script>
     {{-- <script rel="preload" defer src="/js/contact-form.js"></script> --}}
-    <script rel="preload" defer src="/js/jquery.sticky.js"></script>
-    <script rel="preload" defer src="/js/jquery.counterup.min.js"></script>
+    <script rel="preload" async src="/js/jquery.sticky.js"></script>
+    <script rel="preload" async src="/js/jquery.counterup.min.js"></script>
     {{-- <script rel="preconnect" defer src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script> --}}
     <!--===== ACTIVE JS=====-->
-    <script rel="preload" defer src="/js/main.js"></script>
+    <script rel="preload" async src="/js/main.js"></script>
     {{-- <script rel="preload" defer type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBbtRphsSDPc0-N5CasZHsGHwd2yjB_c_k&sensor=false"></script> --}}
     {{-- <script rel="preload" defer src="/js/maps.active.js"></script> --}}
 
-    <script rel="preload" defer type="text/javascript">
+    <script rel="preload" async type="text/javascript">
         $( function(){
             $('#exampleModalCenter').modal();
         });
@@ -37,7 +37,7 @@
             $('#modalerror').modal();
         })
     </script>
-    <script type="text/javascript">
+    <script async type="text/javascript">
     
         var map = L.map('mapid', { fadeAnimation: false }).setView([9.541518, -13.675301], 18);
         

9c37df4643294da5f88843168a47eefd74cf7adb
DynoSlice <alphakaba5@gmail.com>
2020-12-20 00:04:28 +0100

    async js script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,16 +14,16 @@
 
     <!--====== PLUGINS JS ======-->
     {{-- laisser async --}}
-    <script   src="/js/vendor/jquery.easing.1.3.js"></script>
-    <script   src="/js/vendor/jquery-migrate-1.2.1.min.js"></script>
-    <script   src="/js/vendor/jquery.appear.js"></script>
+    <script async   src="/js/vendor/jquery.easing.1.3.js"></script>
+    <script  async  src="/js/vendor/jquery-migrate-1.2.1.min.js"></script>
+    <script  async src="/js/vendor/jquery.appear.js"></script>
     <script   src="/js/owl.carousel.min.js"></script>
     <script   src="/js/stellar.js"></script>
     <script   src="/js/wow.min.js"></script>
     <script   src="/js/stellarnav.min.js"></script>
     <script   src="/js/jquery.sticky.js"></script>
     <script   src="/js/jquery.counterup.min.js"></script>
-    <script type="text/javascript"  src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
+    {{-- <script type="text/javascript"  src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script> --}}
     <!--===== ACTIVE JS=====-->
     <script src="/js/main.js"></script>
     {{-- <script async type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBbtRphsSDPc0-N5CasZHsGHwd2yjB_c_k&sensor=false"></script> --}}

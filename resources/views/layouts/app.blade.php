c4b4875ca949fb38f304f8777459eeda759420a0
DynoSlice <alphakaba5@gmail.com>
2020-12-20 22:30:11 +0100

    Remove listener bug firefox

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,7 @@
             $('#modalerror').modal();
         })
 
-        document.addEventListener('touchstart', onTouchStart, {passive: true});
+        // document.addEventListener('touchstart', onTouchStart, {passive: true});
     </script>
     
         
